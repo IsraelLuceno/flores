1ca69defc68185e6f0f2ea853a0fd4bc4b9d2ba8
IsraelLuceno <orkzera@gmail.com>
2026-07-27 15:22:25 -0300

    Change product page to plant page with updated content

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ function App() {
 
   return (
     <>
-      <h1 className={e.titulo}>Página de produtos</h1>
+      <h1 className={e.titulo}>Página de plantas</h1>
       <main className={e.principal}>
         {produtos.map((item) => {
           return(
             <div key={item.id} className={e.card}>
-              <Card imagem={item.image} nome={item.name} descricao={item.desc} valor={item.value}/>
+              <Card
+                plant={item.plant}
+                espec={item.espec}
+                fami={item.fami}
+                orige={item.orige}
+              />
             </div>
           )
         })}
-
+        
       </main>
     </>
   )
